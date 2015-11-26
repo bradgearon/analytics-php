a46e329e85ec8bb6ad5852d7a330e112b626e600
Brad Gearon <bradgearon@gmail.com>
2015-11-25 20:23:11 -0600

    **Shipped -- Add .gitignore

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Hello, world!</title>
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700' rel='stylesheet' type='text/css'>
+	<style type="text/css">
+	body {
+		background-color: white;
+		text-align: center;
+		padding: 50px;
+		font-family: "Open Sans","Helvetica Neue",Helvetica,Arial,sans-serif;
+	}
+	</style>
+</head>
+<body>
+	<img id="logo" src="logo_blue.png" alt="logo.png"/>
+	<h1><?php echo "Hello, World!!"; ?></h1>
+</body>
+</html>
